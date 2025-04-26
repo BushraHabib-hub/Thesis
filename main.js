ff0278051b34a7c176c838eea494f50aac769a8c
Bushra <you@example.com>
2025-04-26 23:47:25 +0500

    added commands

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function executeCommand(cmd) {
         });        
     });
 }
-async function writeCodeToLib(code) {
+async function writeCodeToLib(filePath, code) {
     try {
         await writeFileAsync(filePath, code);
         console.log(`File written to ${filePath}`);
@@ -45,9 +45,9 @@ async function addDepsToCargo(foldername) {
                 dependenciesFound = true;
                 // Add dependencies if the section is found
                 modifiedLines.push('wasm-bindgen = "0.2"');
-                modifiedLines.push('serde = { version = "1.0", features = ["derive"] }');
-                modifiedLines.push('serde_json = "1.0"');
-                modifiedLines.push('serde-wasm-bindgen = "0.5"');
+                // modifiedLines.push('serde = { version = "1.0", features = ["derive"] }');
+                // modifiedLines.push('serde_json = "1.0"');
+                // modifiedLines.push('serde-wasm-bindgen = "0.5"');
             } else if (lines[i].trim() === '[lib]') {
                 libFound = true;
                 modifiedLines.push('crate-type = ["cdylib", "rlib"]');
@@ -73,9 +73,25 @@ async function process(foldername, filename, code) {
     try {
         await executeCommand("cargo new " + foldername + " --lib");
         const filePath = `${foldername}/src/lib.rs`;
-        await writeCodeToLib(code);
+        await writeCodeToLib(filePath, code);
         await addDepsToCargo(foldername);
         
+        // Check if wasm32 target is installed, if not install it
+        try {
+            console.log("Checking for web");
+            await executeCommand("rustup target list --installed | findstr web");
+        } catch (e) {
+            console.log("web target not found, installing...");
+            await executeCommand("rustup target add web");
+        }
+        
+        // Add compilation step
+        console.log("Building WebAssembly module...");
+        // Change to the project directory and build it
+        await executeCommand(`cd ${foldername} && wasm-pack   build --target web`);
+        console.log("WebAssembly module built successfully");
+        
+        return true;
     } catch (error) {
         console.error(error);
         return false;
